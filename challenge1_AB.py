ca41ab0795ed5364acbc4b55882ffe0b24e709d9
Aquiba Benarroch <aquibabenarroch@gmail.com>
2021-06-28 14:41:49 -0400

    parts 1-2

--- challenge1_AB.py
+++ challenge1_AB.py
@@ -2,8 +2,7 @@
 import csv
 from pathlib import Path
 
-"""Part 1: Automate the Calculations.
-"""
+"""Part 1: Automate the Calculations"""
 
 loan_costs = [500, 600, 200, 1000, 450]
 
@@ -31,25 +30,14 @@ loan = {
     "future_value": 1000,
 }
 
-# @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
-# Print each variable.
 future_value = loan.get("future_value")
 remaining_months = loan.get("remaining_months")
 print(future_value, remaining_months)
 
-
-# @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
-# Use a minimum required return of 20% as the discount rate.
-#   You'll want to use the **monthly** version of the present value formula.
-#   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 hurdle_rate = 0.2
 present_value = future_value/(1+hurdle_rate/12)**loan.get("remaining_months")
 print(present_value)
 
-# If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
-# @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
-#    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
-#    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 if present_value >= loan.get("loan_price"):
     print("The loan is worth buying")
 else:
